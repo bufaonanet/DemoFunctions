e1d72f97c7254f06648b7bac5843e17faef9dcba
bufaonanet <douglas.bufaonanet@outlook.com>
2022-04-04 23:05:13 -0300

    Criando função de dispara email

--- DemoFunctions/EmailLicenseFile.cs
+++ DemoFunctions/EmailLicenseFile.cs
@@ -1,6 +1,8 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using SendGrid.Helpers.Mail;
+using System;
+using System.Text.RegularExpressions;
 
 namespace DemoFunctions
 {
@@ -8,14 +10,25 @@ namespace DemoFunctions
     {
         [FunctionName("EmailLicenseFile")]
         public static void Run(
-            [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")]string  licenseFileContents, 
+            [BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")] string licenseFileContents,
             [SendGrid(ApiKey = "SenGridApiKey")] out SendGridMessage message,
-            string name, 
+            string name,
             ILogger log)
         {
-            message = new SendGridMessage();
+            var email = Regex.Match(licenseFileContents, @"^Email\:\ (.+)$", RegexOptions.Multiline).Groups[1].Value;
+            log.LogInformation($"Got order from {email} \n Lincense file Name {name}");
 
-            log.LogInformation($"");
+            message = new SendGridMessage();
+            message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
+            message.AddTo(email);
+
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContents);
+            var base64 = Convert.ToBase64String(plainTextBytes);
+
+            message.AddAttachment(name, base64, "text/plain");
+            message.Subject = "Your lincese file";
+            message.HtmlContent = "Thank you for your order";
+
         }
     }
 }
